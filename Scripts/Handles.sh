0269255fa4f80369f8ab5169925e990a8f777a9a
jimuruxue <1611575262@qq.com>
2026-05-16 20:56:24 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -118,15 +118,15 @@ else
 fi
 
 #修复quickstart温度显示
-#QUICKSTART_FILE="../package/luci-app-quickstart/luasrc/controller/istore_backend.lua"
-#QUICKSTART_URL="https://gist.githubusercontent.com/puteulanus/1c180fae6bccd25e57eb6d30b7aa28aa/raw/istore_backend.lua"
+QUICKSTART_FILE="../package/luci-app-quickstart/luasrc/controller/istore_backend.lua"
+QUICKSTART_URL="https://gist.githubusercontent.com/puteulanus/1c180fae6bccd25e57eb6d30b7aa28aa/raw/istore_backend.lua"
 
-#if wget -O "$QUICKSTART_FILE" "$QUICKSTART_URL"; then
-#    echo "quickstart温度显示已成功修复！"
-#else
-#    echo "错误：无法下载quickstart文件,请检查URL和网络连接。"
-#	exit 1
-#fi
+if wget -O "$QUICKSTART_FILE" "$QUICKSTART_URL"; then
+    echo "quickstart温度显示已成功修复！"
+else
+    echo "错误：无法下载quickstart文件,请检查URL和网络连接。"
+	exit 1
+fi
 
 # 安装opkg distfeeds
 #emortal_def_dir="$GITHUB_WORKSPACE/wrt/package/emortal/default-settings"
